d9473cd7196318517adb50d35be5b4e5a6786449
sznvn <sznv.na@gmail.com>
2021-10-03 17:16:48 +0300

    downloading all files saving with ordinal names

--- downloader.py
+++ downloader.py
@@ -1,38 +1,24 @@
 import wget
 import requests
-#import urllib.request
 from bs4 import BeautifulSoup
 
-URL = 'https://www.muiv.ru/studentu/fakultety-i-kafedry/fakultet-it/raspisaniya/'
-page = requests.get(URL).text
-soup = BeautifulSoup(page, "html.parser").find_all('p', 'm-doc__name')
 docs = []
-names = []
+faculties = ['fakultet-it/raspisaniya/',
+             'fakultet-upravleniya/raspisaniya-zanyatiy-fakulteta-upravlekniya.php',
+             'fakultet-ekonomiki-i-finansov/raspisaniya-zanyatiy-fakulteta-ekonomiki-i-finansov.php',
+             'yuridicheskiy-fakultet/raspisaniya-zanyatiy-yuridicheskogo-fakulteta.php']
+
+for faculty in faculties:
+    url = 'https://www.muiv.ru/studentu/fakultety-i-kafedry/' + faculty
+    page = requests.get(url).text
+    tags = BeautifulSoup(page, "html.parser").find_all('p', 'm-doc__name')
+    for tag in tags:
+        gateway = 'https://www.muiv.ru' + tag.find('a').get('href')
+        if gateway.endswith('xls'):
+            docs.append(gateway)
+
 i = 0
-# asoup = soup.find_all('p', 'm-doc__name')
-
-for a in soup:
-    names.append(a)
-    url = 'https://www.muiv.ru' + a.find('a').get('href')
-    if(url.endswith('xls')):
-        docs.append(url)
-
-print(docs)
-
-#wget.download('https://pythonworld.ru/m/img/python-3.png', 'C:/Users/sznvn/PycharmProjects/XLSparser/files/cat.png')
 for doc in docs:
     i += 1
     print(doc)
-    wget.download(doc, 'C:/Users/sznvn/PycharmProjects/XLSparser/files/' + str(i) + '.xls')
-
-#urllib.request.urlretrieve('https://pythonworld.ru/m/img/python-3.png', 'C:/Users/sznvn/PycharmProjects/XLSparser/files/cat.png')
-
-
-#govno peredelat'
-#for doc in docs:
-#    name = doc.split('/')[6]
-#    print(name)
-#    dir = '/output' + name[0]
-#    file = open(dir, 'w')
-#    file.write(requests.get(doc).text)
-#print(page)
+    wget.download(doc, './files/' + str(i) + '.xls')
